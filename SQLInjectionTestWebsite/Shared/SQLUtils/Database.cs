a57c121d6d230bd53fff41f12f30f71e7c2369e9
FishArmy100 <87653948+FishArmy100@users.noreply.github.com>
2023-01-31 16:08:42 -0500

    Changed to using sqlite

---
+++ SQLInjectionTestWebsite/Shared/SQLUtils/Database.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace SQLInjectionTestWebsite.Shared.SQLUtils
+{
+    public class SqliteDatabase
+    {
+        public SqliteDatabase(string name)
+        {
+			string cs = "Data Source=:memory:";
+			string stm = "SELECT SQLITE_VERSION()";
+
+			using var con = new SqliteConnection(cs);
+			con.Open();
+
+			using var cmd = new SqliteCommand(stm, con);
+			string? version = cmd.ExecuteScalar()?.ToString();
+
+			Console.WriteLine($"SQLite version: {version}");
+		}
+    }
+}
