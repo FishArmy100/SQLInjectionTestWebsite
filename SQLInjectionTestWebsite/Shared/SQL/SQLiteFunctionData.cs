6a5960c0888a293d2c14503c5e14f99355e69822
FishArmy100 <87653948+FishArmy100@users.noreply.github.com>
2023-02-15 15:29:32 -0500

    Testing custom SQLite functions

---
+++ SQLInjectionTestWebsite/Shared/SQL/SQLiteFunctionData.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.SQLite;
+
+namespace SQLInjectionTestWebsite.Shared.SQL
+{
+	public readonly struct SQLiteFunctionData
+	{
+		public readonly string FunctionName;
+		public readonly int ArgumentCount;
+		public readonly FunctionType FunctionType;
+
+		public readonly Func<object[], object> Function;
+
+		public SQLiteFunctionData(string functionName, int argumentCount, FunctionType functionType, Func<object[], object> function)
+		{
+			FunctionName = functionName;
+			ArgumentCount = argumentCount;
+			FunctionType = functionType;
+			Function = function;
+		}
+
+		public SQLiteFunctionAttribute ToAttribute()
+		{
+			return new SQLiteFunctionAttribute(FunctionName, ArgumentCount, FunctionType);
+		}
+	}
+}
---
+++ SQLInjectionTestWebsite/Shared/SQL/SQLiteFunctionGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System.Data.SQLite;
+
+namespace SQLInjectionTestWebsite.Shared.SQL
+{
+	public static class SQLiteFunctionGenerator
+	{
+		public static void BindFunction(SQLiteConnection connection, SQLiteFunctionData data)
+		{
+			SQLiteFunctionAttribute attribute = data.ToAttribute();
+			SQLiteFunctionWrapper wrapper = new SQLiteFunctionWrapper(data.Function, data.FunctionName, data.ArgumentCount);
+			connection.Open();
+			connection.BindFunction(attribute, wrapper);
+			bool removed = connection.UnbindFunction(data.ToAttribute());
+			connection.Close();
+			Console.WriteLine($"Function removed: {removed}");
+		}
+
+		private class SQLiteFunctionWrapper : SQLiteFunction
+		{
+			private readonly Func<object[], object> m_Function;
+			private readonly string m_FunctionName;
+			private readonly int m_ArgCount;
+			public SQLiteFunctionWrapper(Func<object[], object> function, string functionName, int argCount)
+			{
+				m_Function = function;
+				m_FunctionName = functionName;
+				m_ArgCount = argCount;
+			}
+
+			public override object Invoke(object[] args)
+			{
+				if(m_ArgCount != args.Length)
+					throw new ArgumentException($"Function {m_FunctionName} requires {m_ArgCount} arguments, but was passed {args.Length} arguments.");
+
+				return m_Function(args);
+			}
+		}
+	}
+}
