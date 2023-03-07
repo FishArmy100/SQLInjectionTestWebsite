9bd1402089a898f4402004bb0d7465446eba1551
FishArmy100 <87653948+FishArmy100@users.noreply.github.com>
2023-03-07 12:02:02 -0500

    Working on cart

---
+++ SQLInjectionTestWebsite/Shared/CartItemParser.cs
@@ -0,0 +1,16 @@
+﻿namespace SQLInjectionTestWebsite.Shared
+{
+	public static class CartItemParser
+	{
+		public const string Seperator = ";";
+
+		public static List<ProductInfo> ParseProducts(string cart)
+		{
+			string[] productIds = cart.Split(Seperator);
+			List<ProductInfo> products = WebsiteDatabase.GetAllProducts()
+				.Where(p => productIds.Contains(p.ID))
+				.ToList();
+			return products;
+		}
+	}
+}
